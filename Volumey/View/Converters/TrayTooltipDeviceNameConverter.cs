15b6fc4226c688b791443a6d41cbe0932f9be898
Stas Geft <stas.geft@gmail.com>
2021-04-18 22:37:32 +0300

    Separate tray icon's tooltip into several text blocks

---
+++ Volumey/View/Converters/TrayTooltipDeviceNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Volumey.Localization;
+
+namespace Volumey.View.Converters
+{
+	public class TrayTooltipDeviceNameConverter : IMultiValueConverter
+	{
+		private const string NoDeviceMessageKey = "Error_NoDevice";
+		
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo cltr)
+		{
+			if(values == null)
+				return Binding.DoNothing;
+
+			if(values[0] is true) //bool DeviceProvider.NoOutputDevices
+				return TranslationSource.Instance[NoDeviceMessageKey];
+			
+			if(values[1] is string deviceName)
+				return $"{deviceName} - ";
+
+			return Binding.DoNothing;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo cltr)
+			=> null;
+	}
+}
---
+++ Volumey/View/Converters/TrayTooltipDeviceVolumeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Volumey.View.Converters
+{
+	public class TrayTooltipDeviceVolumeConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo cltr)
+		{
+			if(values == null)
+				return Binding.DoNothing;
+
+			if(values[0] is true) //bool DeviceProvider.NoOutputDevices
+				return string.Empty;
+
+			if(values[1] is int volume)
+				return $"{volume.ToString(CultureInfo.InvariantCulture)}%";
+
+			return Binding.DoNothing;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo cltr)
+			=> null;
+	}
+}
