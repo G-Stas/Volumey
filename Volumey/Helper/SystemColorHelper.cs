e3ef105d34ca9f6784ed7bf50dbea8925efecff2
Stas Geft <stas.geft@gmail.com>
2021-08-30 06:18:41 +0300

    Pull current Windows color mode from registry

--- Volumey/Helper/SystemColorHelper.cs
+++ Volumey/Helper/SystemColorHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Globalization;
-using Windows.UI.ViewManagement;
+using log4net;
 using Microsoft.Win32;
 using Volumey.DataProvider;
 
@@ -11,8 +10,9 @@ namespace Volumey.Helper
         internal static Action<AppTheme> WindowsThemeChanged;
         internal static AppTheme WindowsTheme { get; private set; }
 
-		private static readonly UISettings UiSettings = new UISettings();
-		
+        private static ILog _logger;
+        private static ILog Logger => _logger ??= LogManager.GetLogger(typeof(SystemColorHelper));
+
 		static SystemColorHelper()
 		{
 			SystemEvents.UserPreferenceChanged += OnUserPreferencesChanged;
@@ -25,15 +25,32 @@ namespace Volumey.Helper
 		/// <returns></returns>
 		private static AppTheme GetWindowsTheme()
 		{
-			var color = UiSettings.GetColorValue(UIColorType.Background);
-			if(color.ToString(CultureInfo.InvariantCulture).Equals("#FF000000", StringComparison.InvariantCultureIgnoreCase))
-				return AppTheme.Dark;
+			try
+			{
+				var systemUsesLightTheme = (int) Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize\", "SystemUsesLightTheme", -1);
+				if(systemUsesLightTheme != -1)
+					return systemUsesLightTheme == 1 ? AppTheme.Light : AppTheme.Dark;
+			}
+			catch(Exception e)
+			{
+				Logger.Error($"Failed to get SystemUsesLightTheme key from registry", e);
+			}
+			try
+			{
+				var appsUseLightTheme = (int) Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize\", "AppsUseLightTheme", -1);
+				if(appsUseLightTheme != -1)
+					return appsUseLightTheme == 1 ? AppTheme.Light : AppTheme.Dark;
+			}
+			catch(Exception e)
+			{
+				Logger.Error($"Failed to get AppsUseLightTheme key from registry", e);
+			}
 			return AppTheme.Light;
 		}
 		
 		private static void OnUserPreferencesChanged(object sender, UserPreferenceChangedEventArgs e)
 		{
-			// This event categorizes the change as a UserPreferenceCategory.General type change
+			// Theme change event categorizes the change as a UserPreferenceCategory.General type change
 			// that is the default when the old logic does not know what has changed
 			if(e.Category == UserPreferenceCategory.General)
 			{
