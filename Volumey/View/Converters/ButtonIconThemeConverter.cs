9b10d8f62a6316ffbe0fd494cdfca03fff85b5fd
Stas Geft <stas.geft@gmail.com>
2021-09-13 21:44:03 +0300

    Add ko-fi button to settings view

---
+++ Volumey/View/Converters/ButtonIconThemeConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using Volumey.DataProvider;
+
+namespace Volumey.View.Converters
+{
+	public class ButtonIconThemeConverter : IMultiValueConverter
+	{
+		private BitmapImage mug;
+		private BitmapImage MugIcon => mug ??= App.Current.FindResource("Mug") as BitmapImage;
+		
+		private BitmapImage mugEmpty;
+		private BitmapImage MugEmptyIcon => mugEmpty ??= App.Current.FindResource("MugEmpty") as BitmapImage;
+
+		private BitmapImage mugDark;
+		private BitmapImage MugIconDark => mugDark ??= App.Current.FindResource("MugDark") as BitmapImage;
+		
+		private BitmapImage mugEmptyDark;
+		private BitmapImage MugEmptyIconDark => mugEmptyDark ??= App.Current.FindResource("MugEmptyDark") as BitmapImage;
+
+		private BitmapImage lightGit;
+		private BitmapImage LightGitIcon => lightGit ??= App.Current.FindResource("GitHub") as BitmapImage;
+
+		private BitmapImage darkGit;
+		private BitmapImage DarkGitIcon => darkGit ??= App.Current.FindResource("GitHubDark") as BitmapImage;
+
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			try
+			{
+				var theme = AppTheme.Light;
+				if(values[0] is AppTheme selectedTheme)
+				{
+					if(selectedTheme == AppTheme.System && values[1] is AppTheme windowsTheme)
+						theme = windowsTheme;
+					else
+						theme = selectedTheme;
+				}
+
+				if(parameter.Equals("Mug") && values[2] is bool isMouseOver)
+					return GetMugIcon(theme, isMouseOver);
+				if(parameter.Equals("Git"))
+					return theme == AppTheme.Dark ? LightGitIcon : DarkGitIcon;
+			}
+			catch { }
+			return Binding.DoNothing;
+		}
+
+		private BitmapImage GetMugIcon(AppTheme theme, bool isMouseOver)
+			=> theme == AppTheme.Dark ? GetLightMugIcon(isMouseOver) : GetDarkMugIcon(isMouseOver);
+
+		private BitmapImage GetDarkMugIcon(bool isMouseOver)
+			=> isMouseOver ? MugIconDark : MugEmptyIconDark;
+
+		private BitmapImage GetLightMugIcon(bool isMouseOver)
+			=> isMouseOver ? MugIcon : MugEmptyIcon;
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+			=> new object[] { };
+	}
+}
--- Volumey/ViewModel/SettingsViewModel.cs
+++ Volumey/ViewModel/SettingsViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Volumey.ViewModel.Settings;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors.Core;
+using Volumey.ViewModel.Settings;
 
 namespace Volumey.ViewModel
 {
@@ -9,6 +13,8 @@ namespace Volumey.ViewModel
         public OpenHotkeyViewModel OpenHotkeyViewModel { get; }
         public VolumeLimitViewModel VolumeLimitViewModel { get; }
         public LangSettings LangSettings { get; }
+        public ICommand GitHubCommand { get; }
+        public ICommand TipCommand { get; }
         
         public SettingsViewModel()
         {
@@ -17,6 +23,14 @@ namespace Volumey.ViewModel
             this.DeviceHotkeysViewModel = new DeviceHotkeysViewModel();
             this.OpenHotkeyViewModel = new OpenHotkeyViewModel();
             this.VolumeLimitViewModel = new VolumeLimitViewModel();
+            this.GitHubCommand = new ActionCommand(async () => { await OpenWebPage("https://github.com/G-Stas/Volumey"); });
+            this.TipCommand = new ActionCommand(async () => { await OpenWebPage("https://ko-fi.com/stasg"); });
+        }
+        
+        private async Task OpenWebPage(string url)
+        {
+            try { await Windows.System.Launcher.LaunchUriAsync(new Uri(url)); }
+            catch { }
         }
     }
 }
