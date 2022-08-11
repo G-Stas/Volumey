7d22fd7820d1a86e1cf7a97fa2390aeaa765e950
Stas Geft <stas.geft@gmail.com>
2022-08-12 01:16:56 +0300

    Don't display drop down button for default device in the mixer view

--- Volumey/View/Converters/DeviceIsDefaultToVisibilityPropertiesConverter.cs
+++ Volumey/View/Converters/DeviceIsDefaultToVisibilityPropertiesConverter.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using Volumey.Model;
 
 namespace Volumey.View.Converters
 {
-	public class DeviceIsDefaultToEnabledConverter : IMultiValueConverter
+	public class DeviceIsDefaultToVisibilityPropertiesConverter : IMultiValueConverter
 	{
 		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
 			if(values != null && values[0] is OutputDeviceModel selectedDevice && values[1] is OutputDeviceModel currentDefaultDevice)
-				return selectedDevice != currentDefaultDevice;
-			return false;
+			{
+				if(targetType == typeof(bool))
+					return selectedDevice != currentDefaultDevice;
+				if(targetType == typeof(Visibility))
+					return selectedDevice == currentDefaultDevice ? Visibility.Collapsed : Visibility.Visible;
+			}
+			return Binding.DoNothing;
 		}
 
 		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
